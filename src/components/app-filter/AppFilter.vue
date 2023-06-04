52cc158551c28a8094a15528dfd4dd2ac7b1a668
izzat <izzat@gmail.com>
2023-06-04 23:19:34 +0500

    first commit

---
+++ src/components/app-filter/AppFilter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="btn-group">
+        <button type="button" class="btn btn-dark">Barcha kinolar</button>
+        <button type="button" class="btn btn-outline-dark">
+            Mashhur kinolar
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+            Ko'p ko'rilgan kinolar
+        </button>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
